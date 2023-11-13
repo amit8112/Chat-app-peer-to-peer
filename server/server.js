0022417c77f125f209cbdea3dcc3b6cfd94db4e2
Amit Tripathi <amittripathir@gmail.com>
2023-11-13 19:04:44 +0530

    Env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config(); // Load environment variables from .env file
 const app = express();
+
 const PORT = process.env.PORT || 3001; // Use any port you prefer
 
 // Serve static files from the 'build' folder
